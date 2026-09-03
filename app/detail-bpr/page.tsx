686c2a9d82acca11237ec7e1c39e36201e69d35e
afif <milhamafif027@gmail.com>
2026-09-03 11:17:12 +0700

    update: revisi bagian perbandingan

--- app/detail-bpr/page.tsx
+++ app/detail-bpr/page.tsx
@@ -599,7 +599,7 @@ export default function DetailBPRPage() {
               {/* 5. PERMODALAN & MIAPB */}
               <div className="bg-slate-50 p-4 rounded-xl border border-slate-200 space-y-2 flex flex-col justify-between h-80">
                 <div className="text-xs font-bold text-slate-700">
-                  5. PERMODALAN & MIAPB ({selectedYear}) - %
+                  5. PERMODALAN ({selectedYear}) - %
                 </div>
                 <div className="flex-1 w-full">
                   <ResponsiveContainer width="100%" height="100%">
